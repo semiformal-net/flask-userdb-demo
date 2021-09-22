99bf2acf6103bfbe5a455455027f8eafd8645ba2
admin <admin@semiformal.net>
2021-09-21 20:37:36 -0400

    remove sqlite fallback

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -4,7 +4,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
     GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
